d45ed134776547adb91dab1b54f41137e4851108
Ubuntu <mkudryashov@automation.local>
2025-06-08 14:17:55 +0300

    add homework-24

---
+++ homework-24/vars.tf
@@ -0,0 +1,68 @@
+variable "virtual_machines" {
+    default = {
+        "ns01" = {
+            vmid = 201
+            hostname = "ns01"
+            description = "ns01"
+            tags = "terraform,linux,ubuntu,otus"
+            vm_state = "running"
+            protection = "false"
+            pool = "otus"
+            ip_address = "10.10.5.101/24"
+            gateway = "10.10.5.254",
+            target_node = "pve",
+            cpu_cores = 1,
+            memory = "1024",
+            hdd_size = "10G",
+            vm_template = "linux-alma9.5-cloud",
+        }
+        "ns02" = {
+            vmid = 202
+            hostname = "ns02"
+            description = "ns02"
+            tags = "terraform,linux,ubuntu,otus"
+            vm_state = "running"
+            protection = "false"
+            pool = "otus"
+            ip_address = "10.10.5.102/24"
+            gateway = "10.10.5.254",
+            target_node = "pve",
+            cpu_cores = 1,
+            memory = "1024",
+            hdd_size = "10G",
+            vm_template = "linux-alma9.5-cloud",
+        }
+        "client" = {
+            vmid = 203
+            hostname = "client"
+            description = "client"
+            tags = "terraform,linux,ubuntu,otus"
+            vm_state = "running"
+            protection = "false"
+            pool = "otus"
+            ip_address = "10.10.5.103/24"
+            gateway = "10.10.5.254",
+            target_node = "pve",
+            cpu_cores = 1,
+            memory = "1024",
+            hdd_size = "10G",
+            vm_template = "linux-alma9.5-cloud",
+        }
+        "client2" = {
+            vmid = 204
+            hostname = "client2"
+            description = "client2"
+            tags = "terraform,linux,ubuntu,otus"
+            vm_state = "running"
+            protection = "false"
+            pool = "otus"
+            ip_address = "10.10.5.104/24"
+            gateway = "10.10.5.254",
+            target_node = "pve",
+            cpu_cores = 1,
+            memory = "1024",
+            hdd_size = "10G",
+            vm_template = "linux-alma9.5-cloud",
+        }
+    }
+}
